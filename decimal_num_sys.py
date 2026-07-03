7c74146a851068d140bc1bfe7ddfbb790c34663f
VaghelaHeena <vaghelaheena2005@gmail.com>
2026-07-03 19:16:57 +0530

    Update decimal_num_sys.py
    Added a menu for converting decimal numbers to binary, octal, and hexadecimal.

--- decimal_num_sys.py
+++ decimal_num_sys.py
@@ -1,5 +1,28 @@
-num = int(input("Enter a decimal number: "))
 
-print("Binary =", bin(num))
-print("Octal =", oct(num))
-print("Hexadecimal =", hex(num))
+    num = int(input("Enter decimal number: "))
+
+while True:
+    print("\n----- Decimal Number System Menu -----")
+    print("1. Decimal to Binary")
+    print("2. Decimal to Octal")
+    print("3. Decimal to Hexadecimal")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+
+    if choice == 1:
+        print("Binary =", bin(num))
+
+    elif choice == 2:
+        print("Octal =", oct(num))
+
+    elif choice == 3:
+        print("Hexadecimal =", hex(num))
+      
+    elif choice == 4:
+        print("Program Ended")
+        break
+
+    else:
+        print("Invalid Choice")
